1dd40021b1bd5db26a0e58160086732a4f3c4716
Viktor <fvi7707@gmail.com>
2023-06-13 01:10:07 +0300

    HomePage Layout + new favicon

---
+++ Muson/Muson/Migrations/20230612214501_V.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Muson.Migrations
+{
+    public partial class V : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
