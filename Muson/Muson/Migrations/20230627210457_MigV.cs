d7043ea101555e6f23ddf1434431ee1afa71425d
Viktor <fvi7707@gmail.com>
2023-06-30 00:33:51 +0300

    Home page

---
+++ Muson/Muson/Migrations/20230627210457_MigV.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Muson.Migrations
+{
+    public partial class MigV : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
